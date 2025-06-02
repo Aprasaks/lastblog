43332b475c69da9ad6be0dbce426ac04179133ab
Y.KangHyeok <heavenin24@naver.com>
2025-06-02 09:45:17 +0900

    인라인스타일 변경

--- src/components/MarkdownViewer.jsx
+++ src/components/MarkdownViewer.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
-import 'highlight.js/styles/github-dark.css'; 
+import 'highlight.js/styles/monokai-sublime.css';
 
 export default function MarkdownViewer({ source }) {
   return (
